0efa3040a74fa356c6f3cf21fa0960bcfa201f1c
miki-lauda <52676856+miki-lauda@users.noreply.github.com>
2020-12-14 21:47:18 +0100

    Rename branch

---
+++ project.team9/src/test/java/com/ktsnwt/project/team9/repositories/IGeolocationRepositoryUnitTest.java
@@ -0,0 +1,10 @@
+package com.ktsnwt.project.team9.repositories;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+public class IGeolocationRepositoryUnitTest {
+
+	
+}
