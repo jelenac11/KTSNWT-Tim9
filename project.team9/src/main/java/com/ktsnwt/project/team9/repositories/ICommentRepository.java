17bd70d127750ee7307c3816cff9837b0bad204f
jelenac11 <52001925+jelenac11@users.noreply.github.com>
2020-12-19 15:21:08 +0100

    22,1 Implemented approving comments

--- project.team9/src/main/java/com/ktsnwt/project/team9/repositories/ICommentRepository.java
+++ project.team9/src/main/java/com/ktsnwt/project/team9/repositories/ICommentRepository.java
@@ -3,6 +3,7 @@ package com.ktsnwt.project.team9.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ktsnwt.project.team9.model.Comment;
@@ -11,5 +12,8 @@ import com.ktsnwt.project.team9.model.Comment;
 public interface ICommentRepository extends JpaRepository<Comment, Long> {
 
 	Page<Comment> findByCulturalOfferIdAndApproved(Long id, boolean approved, Pageable pageable);
+
+	@Query(value="SELECT * FROM comments c JOIN cultural_offers co ON c.cultural_offer_id = co.id WHERE co.user_id = ?1 AND NOT c.approved", nativeQuery=true)
+	Page<Comment> findByApprovedFalseAndCulturalOfferUserId(Long id, Pageable pageable);
 
 }
