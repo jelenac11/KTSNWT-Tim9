33d6d88bf739e0f90301370deddfcff9627b4e8c
jelenac11 <52001925+jelenac11@users.noreply.github.com>
2020-12-22 00:09:42 +0100

    Comment Service Unit Test

--- project.team9/src/main/java/com/ktsnwt/project/team9/model/CulturalOffer.java
+++ project.team9/src/main/java/com/ktsnwt/project/team9/model/CulturalOffer.java
@@ -90,4 +90,91 @@ public class CulturalOffer implements Serializable {
 		this.admin = admin;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (active ? 1231 : 1237);
+		result = prime * result + ((admin == null) ? 0 : admin.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(averageMark);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((comments == null) ? 0 : comments.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((geolocation == null) ? 0 : geolocation.hashCode());
+		result = prime * result + ((image == null) ? 0 : image.hashCode());
+		result = prime * result + ((marks == null) ? 0 : marks.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((news == null) ? 0 : news.hashCode());
+		result = prime * result + ((subscribedUsers == null) ? 0 : subscribedUsers.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CulturalOffer other = (CulturalOffer) obj;
+		if (active != other.active)
+			return false;
+		if (admin == null) {
+			if (other.admin != null)
+				return false;
+		} else if (!admin.equals(other.admin))
+			return false;
+		if (Double.doubleToLongBits(averageMark) != Double.doubleToLongBits(other.averageMark))
+			return false;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (comments == null) {
+			if (other.comments != null)
+				return false;
+		} else if (!comments.equals(other.comments))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (geolocation == null) {
+			if (other.geolocation != null)
+				return false;
+		} else if (!geolocation.equals(other.geolocation))
+			return false;
+		if (image == null) {
+			if (other.image != null)
+				return false;
+		} else if (!image.equals(other.image))
+			return false;
+		if (marks == null) {
+			if (other.marks != null)
+				return false;
+		} else if (!marks.equals(other.marks))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (news == null) {
+			if (other.news != null)
+				return false;
+		} else if (!news.equals(other.news))
+			return false;
+		if (subscribedUsers == null) {
+			if (other.subscribedUsers != null)
+				return false;
+		} else if (!subscribedUsers.equals(other.subscribedUsers))
+			return false;
+		return true;
+	}
+
 }
