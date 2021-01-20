b8faf2113b3d8ac8ffc0fda0a40de5b1f354c60c
jelenac11 <52001925+jelenac11@users.noreply.github.com>
2021-01-20 17:59:21 +0100

    Users components testing

--- frontend/src/app/auth/guards/role.guard.spec.ts
+++ frontend/src/app/auth/guards/role.guard.spec.ts
@@ -6,8 +6,21 @@ describe('RoleGuard', () => {
   let guard: RoleGuard;
 
   beforeEach(() => {
+<<<<<<< Updated upstream
     TestBed.configureTestingModule({});
     guard = TestBed.inject(RoleGuard);
+=======
+    const jwtServiceMocked = {
+      getToken: jasmine.createSpy('getToken').and.returnValue({
+        accessToken: 'eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzcHJpbmctc2VjdXJpdHktZXhhbXBsZSIsInN1YiI6ImVtYWlsX2FkcmVzYTFAZ21haWwuY29tIiwiYXVkIjoid2ViIiwiaWF0IjoxNjExMDE0MDk0LCJleHAiOjE2MTEwMTU4OTQsInJvbGUiOiJST0xFX0FETUlOIn0.bl85WgHa1NStDvrlxrWa3P6WO7TUMqcrDDd1WPtxVkOV0gh8Mb0kAcaPU3UzlTR0xsNAl8b08N8rLMTGoJD4HQ',
+        expiresIn: 1800000
+      })
+    };
+    const routerMocked = jasmine.createSpyObj('router', ['navigate']);
+    TestBed.configureTestingModule({
+      providers: [RoleGuard,  {provide: JwtService, useValue: jwtServiceMocked}, {provide: Router, useValue: routerMocked }]
+    });
+>>>>>>> Stashed changes
   });
 
   it('should be created', () => {
